4bec70a75a8d58dd1b9b2412b15dd6371e170ebe
GiftixDev <105500931+GiftixDev@users.noreply.github.com>
2022-05-13 15:31:41 +0200

    Add files via upload

---
+++ award.team.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "750348126363320380",
+        role: "Founder",
+    },
+    {
+        id: "750348126363320380",
+        role: "Developer",
+    },
+        {
+        id: "750348126363320380",
+        role: "Community Manager"
+    },
+    {
+        id: "750348126363320380",
+        role: "Support",
+    },
+
+    
+];
